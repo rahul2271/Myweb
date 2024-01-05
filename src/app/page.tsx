f7d09dc3754dce9eb7d6cfc76b324111d0612b18
makecodefun22 <makecodefun22@gmail.com>
2024-01-05 16:50:57 +0530

    W

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -346,7 +346,7 @@ export default function Home() {
 </div>
 
 <section className="px-2 md:mt-[-80px] py-10 md:px-0">
-<div className="mt-[100px] md:mt-[50px] mx-auto max-w-xl text-center">
+<div className="mt-[10px] md:mt-[50px] mx-auto max-w-xl text-center">
       <div className="mx-auto inline-flex rounded-full bg-mypurple px-4 py-1.5">
         <p className="text-xs md:text-[10px] font-semibold uppercase tracking-widest text-white">
           Reviews...
@@ -386,31 +386,28 @@ export default function Home() {
             “Choosing this web service was a game-changer for my business. Their innovative designs and seamless functionality not only elevated my online presence but also significantly boosted my customer engagement. The team expertise and commitment to excellence made the entire process a breeze. I am thrilled with the results and can confidently say they exceeded my expectations. Highly recommended!”
           </p>
         </blockquote>
-        <figcaption className="mt-10 flex items-center gap-x-6">
+        <figcaption className="mt-10 flex items-center gap-x-6 md:ml-[310px]">
           <div className="isolate flex place-content-center -space-x-2">
-            {/* <img
-              className="relative z-30 inline-block h-5 w-5 rounded-full ring-2 ring-white"
-              src="https://overreacted.io/static/profile-pic-c715447ce38098828758e525a1128b87.jpg"
-              alt="Dan_Abromov"
-            /> */}
             <img
-              className="relative z-20 inline-block h-5 w-5 rounded-full ring-2 ring-white"
+              className="relative z-20 inline-block h-10 w-10 rounded-full ring-2 ring-white"
               src="https://res.cloudinary.com/crunchbase-production/image/upload/c_thumb,h_170,w_170,f_auto,g_faces,z_0.7,b_white,q_auto:eco,dpr_1/smokhfs2uevnppc2bmwl"
               alt="Guillermo_Rauch"
             />
             <img
-              className="relative z-10 inline-block h-5 w-5 rounded-full ring-2 ring-white"
+              className="relative z-10 inline-block h-10 w-10 rounded-full ring-2 ring-white"
               src="https://leerob.io/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Favatar.b1d1472f.jpg&w=256&q=75"
               alt="Lee_Robinson"
             />
             <img
-              className="relative z-0 inline-block h-5 w-5 rounded-full ring-2 ring-white"
+              className="relative z-0 inline-block h-10 w-10 rounded-full ring-2 ring-white"
               src="https://nextjs.org/_next/image?url=https%3A%2F%2Fwww.datocms-assets.com%2F35255%2F1665059775-delba.jpg&w=640&q=75"
               alt="Delba"
             />
           </div>
+          
+
           <div>
-            <p className="font-semibold md:text-[10px] text-white">90+ Clients</p>
+            <p className="font-semibold md:text-[20px] text-white">90+ Clients</p>
           </div>
         </figcaption>
       </figure>
