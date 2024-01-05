6d321db1e71d037f55dc14d62eb9ec32f74a862e
makecodefun22 <makecodefun22@gmail.com>
2024-01-05 15:24:01 +0530

    a

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -6,7 +6,7 @@ export default function Aboutpage(){
         <title>Greetings! I&apos;m Rahul Chauhan, the visionary founder of Rchauhan.</title>
         <meta charset="UTF-8"/>
         {/* <meta name="viewport" content="width=device-width, initial-scale=1.0"/> */}
-        <meta name="description" content="Discover the story behind Rchauhan, a digital services hub founded by Rahul Chauhan. Dive into our journey of digital excellence in web development, graphic design, and content creation."/>
+        <meta name="description" content="Discover the story behind Rchauhan, a digital services hub founded by Rahul Chauhan. Dive into our journey of digital excellence."/>
         <meta name="keywords" content="web development, graphic design, content creation, digital services, social media optimization, social media management, digital marketing, business growth, Rahul Chauhan, rchauhan, innovation, creativity, branding "/>
         <meta name="author" content="Rahul Chauhan"/>
         <meta name="robots" content="index, follow"/>
