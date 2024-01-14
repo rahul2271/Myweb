46fbf52b3e357c0ae98934f2234b60a62580b5d3
makecodefun22 <makecodefun22@gmail.com>
2024-01-14 17:27:35 +0530

    privacy policy

--- src/app/privacypolicy/page.js
+++ src/app/privacypolicy/page.js
@@ -27,57 +27,134 @@ export default function Privacy(){
           content="Learn about how we collect, use, and protect your personal information. Read our Privacy Policy to understand our commitment to your privacy."
         />
         <meta name="twitter:image" content="/logo.png" />
-        <section className="max-w-7xl mx-auto place-content-center text-center">
-            <h1 className="text-[40px] animate-pulse mt-[50px]">Privacy Policy</h1>
-            <div className="mt-[20px]">
+        <div className=" fixed inset-x-0 -top-[0rem] -z-10 transform-gpu overflow-hidden blur-3xl md:-top-[0rem]">
+            <svg
+              className="fixed mt-[50px] left-[calc(50%-11rem)] -z-10 h-[21.1875rem] max-w-none -translate-x-1/2 rotate-[30deg] sm:left-[calc(50%-30rem)] sm:h-[42.375rem]"
+              viewBox="0 0 1155 678"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill="url(#45de2b6b-92d5-4d68-a6a0-9b9b2abad533)"
+                fillOpacity=".3"
+                d="M317.219 518.975L203.852 678 0 438.341l317.219 80.634 204.172-286.402c1.307 132.337 45.083 346.658 209.733 145.248C936.936 126.058 882.053-94.234 1031.02 41.331c119.18 108.451 130.68 295.337 121.53 375.223L855 299l21.173 362.054-558.954-142.079z"
+              />
+              <defs>
+                <linearGradient
+                  id="45de2b6b-92d5-4d68-a6a0-9b9b2abad533"
+                  x1="1155.49"
+                  x2="-78.208"
+                  y1=".177"
+                  y2="474.645"
+                  gradientUnits="userSpaceOnUse"
+                >
+                  <stop stopColor="#9089FC" />
+                  <stop offset={1} stopColor="#FF80B5" />
+                </linearGradient>
+              </defs>
+            </svg>
+          </div>
+        <div className=" fixed inset-x-0 -top-[0rem] -z-10 transform-gpu overflow-hidden blur-3xl md:-top-[0rem]">
+            <svg
+              className="fixed mt-[50px] left-[calc(50%-11rem)] -z-10 h-[21.1875rem] max-w-none -translate-x-1/2 rotate-[30deg] sm:left-[calc(50%-30rem)] sm:h-[42.375rem]"
+              viewBox="0 0 1155 678"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill="url(#45de2b6b-92d5-4d68-a6a0-9b9b2abad533)"
+                fillOpacity=".3"
+                d="M317.219 518.975L203.852 678 0 438.341l317.219 80.634 204.172-286.402c1.307 132.337 45.083 346.658 209.733 145.248C936.936 126.058 882.053-94.234 1031.02 41.331c119.18 108.451 130.68 295.337 121.53 375.223L855 299l21.173 362.054-558.954-142.079z"
+              />
+              <defs>
+                <linearGradient
+                  id="45de2b6b-92d5-4d68-a6a0-9b9b2abad533"
+                  x1="1155.49"
+                  x2="-78.208"
+                  y1=".177"
+                  y2="474.645"
+                  gradientUnits="userSpaceOnUse"
+                >
+                  <stop stopColor="#9089FC" />
+                  <stop offset={1} stopColor="#FF80B5" />
+                </linearGradient>
+              </defs>
+            </svg>
+          </div>
+          <div className="fixed  inset-x-0 -top-[0rem] -z-10 transform-gpu overflow-hidden blur-3xl md:-top-[0rem]">
+            <svg
+              className="mt-[50px] left-[calc(50%-11rem)] -z-10 h-[21.1875rem] max-w-none -translate-x-1/2 rotate-[30deg] sm:left-[calc(50%-30rem)] sm:h-[42.375rem]"
+              viewBox="0 0 1155 678"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill="url(#45de2b6b-92d5-4d68-a6a0-9b9b2abad533)"
+                fillOpacity=".3"
+                d="M317.219 518.975L203.852 678 0 438.341l317.219 80.634 204.172-286.402c1.307 132.337 45.083 346.658 209.733 145.248C936.936 126.058 882.053-94.234 1031.02 41.331c119.18 108.451 130.68 295.337 121.53 375.223L855 299l21.173 362.054-558.954-142.079z"
+              />
+              <defs>
+                <linearGradient
+                  id="45de2b6b-92d5-4d68-a6a0-9b9b2abad533"
+                  x1="1155.49"
+                  x2="-78.208"
+                  y1=".177"
+                  y2="474.645"
+                  gradientUnits="userSpaceOnUse"
+                >
+                  <stop stopColor="#9089FC" />
+                  <stop offset={1} stopColor="#FF80B5" />
+                </linearGradient>
+              </defs>
+            </svg>
+          </div>        
+        <section className="z-10 max-w-7xl md:max-w-6xl mx-auto place-content-center text-center">
+            <div className="mt-[50px] md:mt-[100px] text-gray-500">
                 <p>Last Updated: [06-01-2024]</p>
             </div>
             <div>
-            <h2 className="mt-[20px] text-[30px] font-extrabold">Introduction</h2>
-            <p>Welcome to Rchauhan (&quot;we,&quot; &quot;us,&quot; or &quot;our&quot;). This Privacy Policy is designed to help you understand how we collect, use, disclose, and safeguard your personal information.</p>
+            <h2 className="mt-[20px] text-[30px] font-extrabold md:text-[50px]">Introduction</h2>
+            <p className="md:text-[20px] md:text-gray-200">Welcome to Rchauhan (&quot;we,&quot; &quot;us,&quot; or &quot;our&quot;). This Privacy Policy is designed to help you understand how we collect, use, disclose, and safeguard your personal information.</p>
             </div>
             <div>
-                <h2 className="mt-[20px] text-[30px] font-extrabold">Information We Collect</h2>
+                <h2 className="mt-[20px] text-[30px] font-extrabold md:text-[50px]">Information We Collect</h2>
                 <ol>
-                    <li><h2 className="font-extrabold mt-[20px] text-[20px]">Personal Information :</h2> When you use our website or services, we may collect personal information such as your name, email address, and other contact details.</li>
-                    <li><h2 className="font-extrabold mt-[20px] text-[20px]"> Usage Information :</h2>We collect information about how you interact with our website, including the pages you visit and the actions you take.</li>
-                    <li><h2 className="font-extrabold mt-[20px] text-[20px]">Device Information :</h2>We may collect information about the device you use to access our website, including the device type, operating system, and browser.</li>
+                    <li className="md:text-[20px] md:text-gray-200"><h2 className="font-extrabold mt-[20px] text-[20px] md:text-[30px] md:mt-[40px] md:tracking-wider">Personal Information :</h2> When you use our website or services, we may collect personal information such as your name, email address, and other contact details.</li>
+                    <li className="md:text-[20px] md:text-gray-200"><h2 className="font-extrabold mt-[20px] text-[20px] md:text-[30px] md:mt-[40px] md:tracking-wider"> Usage Information :</h2>We collect information about how you interact with our website, including the pages you visit and the actions you take.</li>
+                    <li className="md:text-[20px] md:text-gray-200"><h2 className="font-extrabold mt-[20px] text-[20px] md:text-[30px] md:mt-[40px] md:tracking-wider">Device Information :</h2>We may collect information about the device you use to access our website, including the device type, operating system, and browser.</li>
                 </ol>
             </div>
             <div>
-                <h2 className="font-extrabold mt-[20px] text-[20px]">How We Use Your Information</h2>
-                <p>We use the information we collect for various purposes, including:</p>
+                <h2 className="font-extrabold mt-[20px] text-[20px] md:text-[30px] md:mt-[40px] md:tracking-wider">How We Use Your Information</h2>
+                <p className="md:text-[20px] md:text-gray-200">We use the information we collect for various purposes, including:</p>
             </div>
             <div>
                 <ol>
-                    <li><h2 className="font-extrabold mt-[20px] text-[20px]">Providing Services: </h2> To deliver the services you request from us.</li>
-                    <li><h2 className="font-extrabold mt-[20px] text-[20px]"> Improving User Experience:</h2>To enhance and personalize your experience on our website.</li>
-                    <li><h2 className="font-extrabold mt-[20px] text-[20px]">Communication:</h2>WTo communicate with you about our services, updates, and promotional offers.</li>
+                    <li className="md:text-[20px] md:text-gray-200"><h2 className="font-extrabold mt-[20px] text-[20px] md:text-[30px] md:mt-[40px] md:tracking-wider">Providing Services: </h2> To deliver the services you request from us.</li>
+                    <li className="md:text-[20px] md:text-gray-200"><h2 className="font-extrabold mt-[20px] text-[20px] md:text-[30px] md:mt-[40px] md:tracking-wider"> Improving User Experience:</h2>To enhance and personalize your experience on our website.</li>
+                    <li className="md:text-[20px] md:text-gray-200"><h2 className="font-extrabold mt-[20px] text-[20px] md:text-[30px] md:mt-[40px] md:tracking-wider">Communication:</h2>WTo communicate with you about our services, updates, and promotional offers.</li>
                 </ol>
             </div>
             <div>
-                <h2 className="font-extrabold mt-[20px] text-[20px]">Cookies and Tracking Technologies</h2>
-                <p>We use cookies and similar technologies to collect information about your browsing activities. You can manage your cookie preferences through your browser settings.</p>
+                <h2 className="font-extrabold mt-[20px] text-[20px] md:text-[30px] md:mt-[40px] md:tracking-wider">Cookies and Tracking Technologies</h2>
+                <p className="md:text-[20px] md:text-gray-200">We use cookies and similar technologies to collect information about your browsing activities. You can manage your cookie preferences through your browser settings.</p>
             </div>
             <div>
-                <h2 className="font-extrabold mt-[20px] text-[20px]">Sharing Your Information</h2>
-                <p>We may share your information with third parties in certain circumstances, such as with service providers or to comply with legal obligations.</p>
+                <h2 className="font-extrabold mt-[20px] text-[20px] md:text-[30px] md:mt-[40px] md:tracking-wider">Sharing Your Information</h2>
+                <p className="md:text-[20px] md:text-gray-200">We may share your information with third parties in certain circumstances, such as with service providers or to comply with legal obligations.</p>
             </div>
             <div>
-                <h2 className="font-extrabold mt-[20px] text-[20px]">Your Choices</h2>
-                <p>You have the right to access, update, or delete your personal information. If you have any questions or requests regarding your data, please contact us at rraahhuullch@gmail.com.</p>
+                <h2 className="font-extrabold mt-[20px] text-[20px] md:text-[30px] md:mt-[40px] md:tracking-wider">Your Choices</h2>
+                <p className="md:text-[20px] md:text-gray-200">You have the right to access, update, or delete your personal information. If you have any questions or requests regarding your data, please contact us at rraahhuullch@gmail.com.</p>
             </div>
             <div>
-                <h2 className="font-extrabold mt-[20px] text-[20px]">Security</h2>
-                <p>We take reasonable measures to protect your personal information from unauthorized access, disclosure, or alteration.</p>
+                <h2 className="font-extrabold mt-[20px] text-[20px] md:text-[30px] md:mt-[40px] md:tracking-wider">Security</h2>
+                <p className="md:text-[20px] md:text-gray-200">We take reasonable measures to protect your personal information from unauthorized access, disclosure, or alteration.</p>
             </div>
             <div>
-                <h2 className="font-extrabold mt-[20px] text-[20px]">Changes to This Privacy Policy</h2>
-                <p>We may update this Privacy Policy to reflect changes in our practices or for other operational, legal, or regulatory reasons. The updated policy will be effective when posted.</p>
+                <h2 className="font-extrabold mt-[20px] text-[20px] md:text-[30px] md:mt-[40px] md:tracking-wider">Changes to This Privacy Policy</h2>
+                <p className="md:text-[20px] md:text-gray-200">We may update this Privacy Policy to reflect changes in our practices or for other operational, legal, or regulatory reasons. The updated policy will be effective when posted.</p>
             </div>
             <div>
-                <h2 className="font-extrabold mt-[20px] text-[20px]">Contact Us</h2>
-                <p>If you have any questions, concerns, or requests regarding this Privacy Policy, please contact us at rraahhuullch@gmail.com.</p>
+                <h2 className="font-extrabold mt-[20px] text-[20px] md:text-[30px] md:mt-[40px] md:tracking-wider">Contact Us</h2>
+                <p className="md:text-[20px] md:text-gray-200">If you have any questions, concerns, or requests regarding this Privacy Policy, please contact us at rraahhuullch@gmail.com.</p>
             </div>
         </section>
         </>
