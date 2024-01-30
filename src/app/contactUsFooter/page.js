6ae343151a8c02910ded479d9d7d9bc936f07c29
Rahul <121254290+rahul2271@users.noreply.github.com>
2024-01-30 13:39:38 +0530

    Update page.js

--- src/app/contactUsFooter/page.js
+++ src/app/contactUsFooter/page.js
@@ -1,6 +1,15 @@
-export default function ContactUs(){
-    return(
-        <>
-        </>
-    )
+"use client";
+import React from 'react'
+import Form from './Form'
+
+
+const page = () => {
+  return (
+    <>
+    <Form/>
+    </>
+    
+  )
 }
+
+export default page
