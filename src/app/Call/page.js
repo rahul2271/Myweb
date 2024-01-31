60f024f2b3ab380847b19eca8ad3243848530af6
makecodefun22 <makecodefun22@gmail.com>
2024-01-31 10:20:36 +0530

    q

--- src/app/Call/page.js
+++ src/app/Call/page.js
@@ -24,7 +24,7 @@ const CallNowButton = () => {
                 type="button"
                 className="rounded-md border md:border-2 border-mypurple px-3 py-2 md:px-6 md:py-3 md:text-[15px] hover:bg-white text-sm font-semibold text-mypurple shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
               >
-                  <Link href="/contactUsFooter">Get Started</Link>
+                  <Link href="/contact">Get Started</Link>
                 
               </button>
             </div>
