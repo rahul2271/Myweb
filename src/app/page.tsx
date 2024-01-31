9ed88d4aef431186dabd63f470003e12e6a28bdb
makecodefun22 <makecodefun22@gmail.com>
2024-01-31 14:23:31 +0530

    e

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -386,20 +386,20 @@ export default function Home() {
             “Choosing this web service was a game-changer for my business. Their innovative designs and seamless functionality not only elevated my online presence but also significantly boosted my customer engagement. The team expertise and commitment to excellence made the entire process a breeze. I am thrilled with the results and can confidently say they exceeded my expectations. Highly recommended!”
           </p>
         </blockquote>
-        <figcaption className="mt-10 flex items-center gap-x-6 md:ml-[310px]">
+        <figcaption className="mt-10 flex mx-auto place-content-center items-center gap-x-6 ">
           <div className="isolate flex place-content-center -space-x-2">
             <img
-              className="relative z-20 inline-block h-10 w-10 rounded-full ring-2 ring-white"
+              className="relative z-20 inline-block h-7 w-7 rounded-full ring-2 ring-white"
               src="https://res.cloudinary.com/crunchbase-production/image/upload/c_thumb,h_170,w_170,f_auto,g_faces,z_0.7,b_white,q_auto:eco,dpr_1/smokhfs2uevnppc2bmwl"
               alt="Guillermo_Rauch"
             />
             <img
-              className="relative z-10 inline-block h-10 w-10 rounded-full ring-2 ring-white"
+              className="relative z-10 inline-block h-7 w-7 rounded-full ring-2 ring-white"
               src="https://leerob.io/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Favatar.b1d1472f.jpg&w=256&q=75"
               alt="Lee_Robinson"
             />
             <img
-              className="relative z-0 inline-block h-10 w-10 rounded-full ring-2 ring-white"
+              className="relative z-0 inline-block h-7 w-7 rounded-full ring-2 ring-white"
               src="https://nextjs.org/_next/image?url=https%3A%2F%2Fwww.datocms-assets.com%2F35255%2F1665059775-delba.jpg&w=640&q=75"
               alt="Delba"
             />
