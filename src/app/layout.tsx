1f2cfb3526520c5025174709ddb45dccf11e9623
makecodefun22 <makecodefun22@gmail.com>
2024-02-01 10:27:06 +0530

    w

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -73,7 +73,7 @@ export default function RootLayout({
             </li> */}
             <li>
               <Link className="hover:text-mypurple" href="/price">
-              Pricing
+              Plans
               </Link>
             </li>            
           </ul>
