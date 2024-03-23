5d263a7e6dee593a99510f730e635da1308321ee
makecodefun22 <makecodefun22@gmail.com>
2024-03-23 17:51:27 +0530

    s

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,6 +39,7 @@ export default function RootLayout({
         <meta name="keywords" content="web development, graphic design, content creation, digital services, social media optimization, social media management, digital marketing, business growth, Rahul Chauhan, RC tech solutions, creativity, branding" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="canonical" href="https://www.rchauhan.in" />
+        <meta property="og:title" content="RC Tech Solutions"/>
         <meta property="og:type" content="website"/>
         <meta property="og:url" content="www.rchauhan.in"/>
         <meta property="og:title" content="RC Tech Solutions - Crafting Digital Excellence"/>
