b1b04be196ccfeb7272ec97a22cc6e74998a7e21
Rahul <121254290+rahul2271@users.noreply.github.com>
2024-05-04 14:50:32 +0530

    Update Form.js

--- src/app/contact/Form.js
+++ src/app/contact/Form.js
@@ -272,7 +272,7 @@ const Form = () => {
                     <input
                       className="flex h-10 w-full rounded-md border border-mypurple bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-700 dark:text-gray-50 dark:focus:ring-gray-400 dark:focus:ring-offset-gray-900"
                       type="file"
-                      id="basic-url"
+                      id="Resume"
                       aria-describedby="basic-addon3 basic-addon4"
                       required
                       accept=".pdf,.doc,.docx"
