6ca2b042d9e8e9a8756bde84ae817d8f0d159770
Rahul <121254290+rahul2271@users.noreply.github.com>
2024-12-26 18:22:03 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,6 +14,17 @@ export default function Home() {
     <main>
 
 <Head>
+  <!-- Hotjar Tracking Code for Site 5251171 (name missing) -->
+<script>
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:5251171,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+</script>
           {/* <link rel="shortcut icon" href={favicon} /> */}
           <link rel="shortcut icon" href="favicon.ico" />
         </Head>
